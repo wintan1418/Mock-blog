595e682672674c6e4584aa3c862870cd8a44ebfb
oluwadare <wintan1418@gmail.com>
2022-04-07 12:17:21 +0100

    Add association of one to many from backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,7 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
 
     respond_to do |format|
       if @article.save
