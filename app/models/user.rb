84a0c471b00d9d87251430ad34eb91d7928b4d0b
oluwadare <wintan1418@gmail.com>
2022-04-07 11:32:53 +0100

    Test email validtaion on console

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ validates :username, presence: true,
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i                     
 validates :email,   presence: true, 
                     uniqueness: {case_sensitive:false},
-                    length: { maximum: 25}
-                    format: { with: VALID_EMAIL_REGEX }
+                    length: { maximum: 25},
+                    format:{ with: VALID_EMAIL_REGEX }
 
 
 end
