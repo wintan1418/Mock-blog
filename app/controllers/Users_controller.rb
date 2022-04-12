28a5e55b4396b5855e23acc2cdbf03090160d5e1
oluwadare <wintan1418@gmail.com>
2022-04-12 14:00:40 +0100

    Add create method and error handling code

--- app/controllers/Users_controller.rb
+++ app/controllers/Users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Wintan Blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
 end
