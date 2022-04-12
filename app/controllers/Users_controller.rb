e76ef461b10db1cdbeabebd8f6ab7d05eab4ccd3
oluwadare <wintan1418@gmail.com>
2022-04-12 18:32:11 +0100

    Finish Users profile page layout

--- app/controllers/Users_controller.rb
+++ app/controllers/Users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
