1924d681ca14c1c42763a4a67afc775fd7a2b940
oluwadare <wintan1418@gmail.com>
2022-04-12 13:45:17 +0100

    Finish signup form

--- app/controllers/Users_controller.rb
+++ app/controllers/Users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def new
-    @user = user.new
+    @user = User.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'articles', to: 'articles#index'
   get 'about', to: 'pages#about'
-  get 'signup', to: 'users#new'
+  
   resources :articles 
+  get 'signup', to: 'users#new'
+  post 'users' , to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
