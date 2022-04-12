055cc3bacfb90e4c22cea2fea2920dce654b8d66
oluwadare <wintan1418@gmail.com>
2022-04-12 16:15:35 +0100

    Add render form for articles listed for User

--- app/controllers/Users_controller.rb
+++ app/controllers/Users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
 
   def show
    @user = User.find(params[:id])
+   @articles = @user.articles
   end
 
   def create
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
       hash = Digest::MD5.hexdigest(email_address)
       size = options[:size]
       gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
-      image_tag(gravatar_url, alt: user.username, class: "rounded-circle")
+      image_tag(gravatar_url, alt: user.username, class: "rounded mx-auto shadow d-block")
     end
 end
