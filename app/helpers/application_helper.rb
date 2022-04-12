32609d3b3dfa95f996f20224a6e66186548be803
oluwadare <wintan1418@gmail.com>
2022-04-12 15:14:49 +0100

    Add gravatar function for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+def gravatar_for(user)
+  email_address = user.email.downcase
+  hash = Digest::MD5.hexdigest(email_address)
+  gravatar_url = "https://www.gravatar.com/avatar/#{hash}"
+  image_tag(gravatar_url, alt: user.username, class: "rounded-circle")
 end
