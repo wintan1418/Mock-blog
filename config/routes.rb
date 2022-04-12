5b7662ecba90d377f7f301a30b6fe846b7f152cc
oluwadare <wintan1418@gmail.com>
2022-04-12 13:52:48 +0100

    Finish first aspect of signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   
   resources :articles 
   get 'signup', to: 'users#new'
-  post 'users' , to: 'users#create'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
