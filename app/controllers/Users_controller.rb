0e4ba80b9218729a383dda39d4a8bbeabb5b74ae
oluwadare <wintan1418@gmail.com>
2022-04-13 14:20:19 +0100

    Set user action in controllers

--- app/controllers/Users_controller.rb
+++ app/controllers/Users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]]
+  
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
+   
   end
 
   def index
@@ -12,7 +14,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
@@ -22,13 +23,13 @@ class UsersController < ApplicationController
   end
 
   def show
-   @user = User.find(params[:id])
    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Wintan Blog #{@user.username}"
       redirect_to articles_path
     else
@@ -39,5 +40,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
