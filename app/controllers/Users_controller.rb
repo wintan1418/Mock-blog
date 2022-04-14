75de464b47e40ee6f5967d8c45feba3de44ed9dc
oluwadare <wintan1418@gmail.com>
2022-04-14 14:43:33 +0100

    Finish  App

--- app/controllers/Users_controller.rb
+++ app/controllers/Users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil 
+    session[:user_id] = nil if @user == current_user?
     flash[:notice] = "Account and all associated articles successfully deleted"
     redirect_to articles_path
   end
@@ -58,7 +58,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user? != @user
+    if current_user? != @user && !current_user?.admin?
       flash[:alert] = "you can only edit you own profile"
       redirect_to user_path
   end
