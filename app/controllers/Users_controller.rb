693185b5d837022c12a1d771b5f311b5267ce35a
oluwadare <wintan1418@gmail.com>
2022-04-14 12:19:07 +0100

    Add destroy method  for users

--- app/controllers/Users_controller.rb
+++ app/controllers/Users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :delete]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user,only: [:edit,:update]
+  before_action :require_same_user,only: [:edit,:update,:destroy]
   
   def new
     @user = User.new
@@ -38,6 +38,15 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if current_user?
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+
   private
 
   def user_params
