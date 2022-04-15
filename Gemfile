91a27153623b2d006df2da01f6d08fa021b7665b
oluwadare <wintan1418@gmail.com>
2022-04-15 10:58:01 +0100

    Initiate test for added categories feature

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '3.0.0'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.6'
 # Use sqlite3 as the database for Active Record
 gem 'will_paginate', '~> 3.3', '>= 3.3.1'
+gem 'rexml', require: false
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_114028) do
+ActiveRecord::Schema.define(version: 2022_04_15_055727) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2022_04_14_114028) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
