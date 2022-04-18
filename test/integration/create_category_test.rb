aee4426123f58f6bd5544e124aebf10e09cacfab
oluwadare <wintan1418@gmail.com>
2022-04-18 13:15:16 +0100

    Add test and categories listing functions

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -5,13 +5,22 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     get "/categories/new"
     assert_response :success
     assert_difference 'Category.count', 1 do
-      post categories_path, params: { category: { name: "sports" } }
+      post categories_path, params: { category: { name: "Sports"} }
       assert_response :redirect
     end
-      follow_redirect!
-      assert_response :success
-      assert_match "sports", response.body
-    
-    
+    follow_redirect!
+    assert_response :success
+    assert_match "Sports", response.body
   end
-end
+
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " "} }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
+  end
